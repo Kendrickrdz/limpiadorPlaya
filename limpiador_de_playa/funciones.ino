ae4b62a20aad7da11f2a53b3d4572b1729a39f5a
Kendrickrdz <kendrickrdz5@gmail.com>
2019-05-02 13:55:47 -0500

    Agregue delay a funciones

--- limpiador_de_playa/funciones.ino
+++ limpiador_de_playa/funciones.ino
@@ -1,4 +1,4 @@
-void adelante(int vel, int vel2){
+void adelante(int vel, int vel2, int t){
   analogWrite(ena, vel);
   analogWrite(enb, vel2);
   digitalWrite(in1,HIGH);
@@ -8,9 +8,10 @@ void adelante(int vel, int vel2){
   digitalWrite(ledrojo,LOW);
   digitalWrite(ledazul,HIGH);
   digitalWrite(ledamarillo,LOW);
+  delay(t);
     }
 
-void derecha(int vel, int vel2){
+void derecha(int vel, int vel2, int t){
     analogWrite(ena, vel);
     analogWrite(enb, vel2);
     digitalWrite(in1,LOW);
@@ -20,9 +21,10 @@ void derecha(int vel, int vel2){
     digitalWrite(ledrojo,LOW);
     digitalWrite(ledazul,LOW);
     digitalWrite(ledamarillo,HIGH);
+    delay(t);
     }
 
- void atras(int vel, int vel2){
+ void atras(int vel, int vel2, int t){
   analogWrite(ena, vel);
   analogWrite(enb, vel2);
   digitalWrite(in1,LOW);
@@ -32,43 +34,46 @@ void derecha(int vel, int vel2){
   digitalWrite(ledrojo,HIGH);
   digitalWrite(ledazul,LOW);
   digitalWrite(ledamarillo,LOW);
+  delay(t);
     }
 
-void izquierda(int vel, int vel2){
+void izquierda(int vel, int vel2, int t){
     analogWrite(ena, vel);
     analogWrite(enb, vel2);
     digitalWrite(in1,HIGH);
     digitalWrite(in2,LOW);
     digitalWrite(in3,LOW);
-    digitalWrite(in4,HIGH); 
+    digitalWrite(in4,HIGH);
+    delay(t); 
     }
-void parar(){
+void parar(int t){
     digitalWrite(in1,LOW);
     digitalWrite(in2,LOW);
     digitalWrite(in3,LOW);
-    digitalWrite(in4,LOW); 
+    digitalWrite(in4,LOW);
+    delay(t);
     }
 
 void esquivar(){
   if (distancia > 100) {
     distancia = 0;
  }
-    derecha(200,200);
+    derecha(200,200,10);
    if (distancia > 30){
-    adelante(250,250);
+    adelante(250,250,10);
     }
     if(distancia > 0 && distancia < 10){
-      atras(100,100);
+      atras(100,100,10);
      // derecha(100,100);
       }
       }
 
  void cesargei(){
-   derecha(200,200);
+   derecha(200,200,10);
    if(distancia < 50 && distancia > 1);
-   adelante(200,200);
+   adelante(200,200,10);
    if(distancia > 15 && distancia < 20);
-   parar();
+   parar(10);
    }
 
 void ultrasonico(){
